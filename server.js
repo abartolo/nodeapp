cc284495b0ce7821abcc60a1a1f9cbdeb797b56e
abartolo <abartolo2@gmail.com>
2015-04-13 19:05:14 -0400

    Added index file

---
+++ server.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Alex on 4/13/2015.
+ */
+
+var http = require("http");
+var fs =  require("fs");
+
+http.createServer(function(req,res){
+
+    fs.readFile('index.html',function(err,data){
+        res.writeHead(200,{'Content-type':'text/html'});
+        res.end(data);
+    });
+
+
+}).listen(1337,'127.0.0.1');
+
+console.log('Webserver has started');
+
+/*
+var http = require("http");
+
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type':'text/plain'});
+    res.end('Hello');
+}).listen(1337,'127.0.0.1');
+
+console.log('Webserver has started');
+*/
