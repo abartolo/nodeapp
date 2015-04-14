21e8b5ab71d65a5aa70f89cf35b7eb29d92cfe06
abartolo <abartolo2@gmail.com>
2015-04-13 20:02:13 -0400

    New modules and tests

---
+++ mysystem.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Alex on 4/13/2015.
+ */
+
+
+var os = require("os");
+
+var message = "Here is some info about your system";
+
+var sysarray = new Array('Type: '+ os.type(),
+                        'Node Verzion: ' + process.version,
+                        'Platform: ' + os.platform(),
+                        'Hostname: ' + os.hostname(),
+                        'Total Memory: ' +os.totalmem(),
+                        'Free Memory: ' + os.freemem(),
+                        'Uptime: '+ os.uptime()
+                        );
+
+console.log(message);
+
+var arraylen = sysarray.length;
+
+i = 0;
+
+
+while(i < arraylen){
+    console.log(sysarray[i]);
+    i++;
+}
---
+++ zipcode.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Alex on 4/13/2015.
+ */
+
+var zipcode = require("zipcode");
+
+console.log(zipcode.lookup('07202'));
+
